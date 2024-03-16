f6a74fefd7b13041113a5d845bdbdf196725b067
samu-rize <6710@holbertonstudents.com>
2024-03-16 14:07:01 +0100

    2. Measure the runtime

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+Flask application
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Display a message to the user"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Display a message to the user"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """Display a message to the user"""
+    if ("_" in text):
+        text = text.replace("_", " ")
+
+    return f"C {text}"
+
+
+if __name__ == '__main__':
+    """Run the main program"""
+    app.run(host='0.0.0.0', port=5000)
