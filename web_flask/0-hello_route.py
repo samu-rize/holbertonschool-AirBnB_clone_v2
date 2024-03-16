106209bd0f479d75906824ee0f77b9be6e8e5544
samu-rize <6710@holbertonstudents.com>
2024-03-16 14:06:14 +0100

    0. The basics of async

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+Flask application
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Display a message to the user"""
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    """Run the main program"""
+    app.run(host='0.0.0.0', port=5000)
